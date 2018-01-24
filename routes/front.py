972604f43fd15ed93259ddc9e96f6a68a2790739
hfcj39 <1256082719@qq.com>
2018-01-24 17:25:56 +0800

    完成评论功能

--- routes/front.py
+++ routes/front.py
@@ -1,9 +1,11 @@
+# coding:utf-8
 from . import index
 from blog import db
 from models.Article import Article
 from models.User import User
+from models.Comment import Comment
 from models.Classes import Classes
-from flask import render_template
+from flask import render_template, abort, request
 
 
 @index.route('/')
@@ -11,12 +13,13 @@ def _index():
     return render_template('index.html')
 
 
-@index.route('/tst')
+@index.route('/about')
 def t():
-    userinfo = User.query.get(1)
-    rst = userinfo.articles
-    print(rst)
-    return render_template('test.html')
+    rst = Article.query.filter_by(classification='about').first()
+    if rst:
+        return render_template('article.html', data=rst)
+    else:
+        abort(500)
 
 
 @index.route('/blog')
@@ -42,4 +45,19 @@ def classification(c_id=1, page=1):
 @index.route('/article/<int:a_id>')
 def get_article(a_id):
     rst = Article.query.get(a_id)
-    return render_template('article.html', data=rst)
+    comment = Comment.query.filter_by(Article_id=a_id)
+    return render_template('article.html', data=rst, comment=comment, a_id=a_id)
+
+
+@index.route('/comment', methods=["POST"])
+def sub_comment():
+    text = request.values.get('text')
+    name = request.values.get('name')
+    mail = request.values.get('mail')
+    a_id = request.values.get('a_id')
+    if name and mail and text and a_id:
+        db.session.add(Comment(Article_id=a_id, name=name, content=text, mail=mail))
+        db.session.commit()
+        return '0'
+    else:
+        return '1'
